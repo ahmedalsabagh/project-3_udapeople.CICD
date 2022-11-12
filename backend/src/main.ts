4b693a5ce0eb5d6cd3aa748567f99d473a8b9772
ahmedalsabagh <94567842+ahmedalsabagh@users.noreply.github.com>
2022-11-12 19:27:09 +0200

    fix backend compile error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -29,6 +29,7 @@ async function bootstrap() {
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
     .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
   const config: ConfigService = app.get('ConfigService');
