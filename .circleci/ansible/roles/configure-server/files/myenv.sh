44435a01af12a10b352951dad2d7402b68293b82
ahmedalsabagh <94567842+ahmedalsabagh@users.noreply.github.com>
2022-11-17 11:26:01 +0200

    run migrations

---
+++ .circleci/ansible/roles/configure-server/files/myenv.sh
@@ -0,0 +1,9 @@
+TYPEORM_CONNECTION=postgres
+TYPEORM_MIGRATIONS_DIR=./src/migrations
+TYPEORM_ENTITIES=./src/modules/domain/**/*.entity.ts
+TYPEORM_MIGRATIONS=./src/migrations/*.ts
+TYPEORM_HOST=udapeople-db.c8xiq8k1wpvv.us-east-1.rds.amazonaws.com
+TYPEORM_PORT=5432
+TYPEORM_USERNAME=postgres
+TYPEORM_PASSWORD=HGcSsdzU7kGx5GZCIFZk
+TYPEORM_DATABASE=postgres
